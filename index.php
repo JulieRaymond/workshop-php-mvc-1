fd4c3c874bf38ecfa2fca18bd517e6e4afa83938
Julie <raymondjulie@proton.me>
2023-10-11 10:35:27 +0200

    "beggining exercice"

--- index.php
+++ index.php
@@ -7,24 +7,5 @@ $statement = $connection->query('SELECT id, title FROM recipe');
 $recipes = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 // Generate the web page
-?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>List of Recipes</title>
-    </head>
-    <body>
-        <h1>List of Recipes</h1>
-        <ul>
-            <?php foreach ($recipes as $recipe): ?>
-            <li>
-                <a href="show.php?id=<?= $recipe['id'] ?>">
-                    <?= $recipe['title'] ?>
-                </a>
-            </li>
-            <?php endforeach; ?>
-        </ul>
-    </body>
-</html>
+
+require __DIR__ . '/src/views/indexRecipe.php';
